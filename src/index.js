ab23431bbe63f1e2a8c89a8f56faca3d07a052af
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-01-27 15:31:53 -0600

    linted code to airbnb JS standards

--- src/index.js
+++ src/index.js
@@ -1,95 +1,86 @@
+/* eslint-disable no-unused-vars */
 const capitalize = (str) => {
   const firstChar = str[0].toUpperCase();
   const newArray = [firstChar, str.slice(1)];
-  return newArray.join("");
+  return newArray.join('');
 };
 
-const allCaps = (str) => {
-  return str.toUpperCase();
-};
+const allCaps = (str) => str.toUpperCase();
 
 const capitalizeWords = (str) => {
-  strArray = str.split(" ");
-  return strArray.map(capitalize).join(" ");
+  const strArray = str.split(' ');
+  return strArray.map(capitalize).join(' ');
 };
 
 const capitalizeHeadline = (str) => {
   const outliers = [
-    "the",
-    "in",
-    "a",
-    "an",
-    "and",
-    "but",
-    "for",
-    "at",
-    "by",
-    "from",
+    'the',
+    'in',
+    'a',
+    'an',
+    'and',
+    'but',
+    'for',
+    'at',
+    'by',
+    'from',
   ];
-  const strArray = str.split(" ");
+  const strArray = str.split(' ');
   const firstWord = capitalize(strArray[0]);
   const restOfWords = strArray
     .slice(1)
-    .map((word) => {
-      return outliers.indexOf(word) !== -1 ? word : capitalize(word);
-    })
-    .join(" ");
+    .map((word) => (outliers.indexOf(word) !== -1 ? word : capitalize(word)))
+    .join(' ');
 
-  return [firstWord, restOfWords].join(" ");
+  return [firstWord, restOfWords].join(' ');
 };
 
 const removeExtraSpaces = (str) => {
   const trimmedStr = str.trim();
-  return trimmedStr.split(/\s+/).join(" ");
+  return trimmedStr.split(/\s+/).join(' ');
 };
 
-const kebobCase = (str, delimiter = "-") => {
+const kebobCase = (str, delimiter = '-') => {
   const goodCodes = [
     32, 45, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 97, 98, 99, 100, 101, 102,
     103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117,
     118, 119, 120, 121, 122,
   ];
   const lowerString = str.toLowerCase();
-  const splitStr = lowerString.split("");
+  const splitStr = lowerString.split('');
   const cleanStr = [];
   for (let i = 0; i < splitStr.length; i += 1) {
-    if (goodCodes.indexOf(splitStr.join("").charCodeAt(i)) !== -1) {
+    if (goodCodes.indexOf(splitStr.join('').charCodeAt(i)) !== -1) {
       cleanStr.push(splitStr[i]);
     }
   }
-  return removeExtraSpaces(cleanStr.join("")).split(" ").join(delimiter);
+  return removeExtraSpaces(cleanStr.join('')).split(' ').join(delimiter);
 };
 
-const snakeCase = (str) => {
-  return kebobCase(str, "_");
-};
+const snakeCase = (str) => kebobCase(str, '_');
 
 const camelCase = (str) => {
-  const strArray = str.trim().split(" ");
+  const strArray = str.trim().split(' ');
   const firstWord = strArray[0].toLowerCase();
-  const restOfWords = strArray.slice(1).map(capitalize).join("");
-  return [firstWord, restOfWords].join("");
+  const restOfWords = strArray.slice(1).map(capitalize).join('');
+  return [firstWord, restOfWords].join('');
 };
 
-const shift = (str, numChars) => {
-  return str.slice(numChars) + str.slice(0, numChars);
-};
+const shift = (str, numChars) => str.slice(numChars) + str.slice(0, numChars);
 
 const makeHashTag = (str) => {
-  const sortedWords = str.split(" ").sort(function (a, b) {
-    return b.length - a.length || a.localeCompare(b);
-  });
+  const sortedWords = str
+    .split(' ')
+    .sort((a, b) => b.length - a.length || a.localeCompare(b));
   if (sortedWords.length < 3) {
-    return sortedWords.map((word) => {
-      return "#" + capitalize(word.toLowerCase());
-    });
+    return sortedWords.map((word) => `#${capitalize(word.toLowerCase())}`);
   }
-  return sortedWords.slice(0, 3).map((word) => {
-    return "#" + capitalize(word.toLowerCase());
-  });
+  return sortedWords
+    .slice(0, 3)
+    .map((word) => `#${capitalize(word.toLowerCase())}`);
 };
 
 const isEmpty = (str) => {
   const trimmedStr = str.trim();
-  return trimmedStr.split(/\s+/).join("").length === 0;
+  return trimmedStr.split(/\s+/).join('').length === 0;
 };
