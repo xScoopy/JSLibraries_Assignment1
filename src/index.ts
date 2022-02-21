2278bb89b9dcedc3b2c4e298584eec1cecf6387f
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-02-21 16:04:28 -0600

    added index.ts as a typescript version of the lib

---
+++ src/index.ts
@@ -0,0 +1,105 @@
+/* eslint-disable no-unused-vars */
+const capitalize = (str: string): string => {
+    if (str.length === 0) {
+      return '';
+    }
+    const firstChar: string = str[0].toUpperCase();
+    const newArray: string[] = [firstChar, str.slice(1)];
+    return newArray.join('');
+  };
+  
+  const allCaps = (str: string): string => str.toUpperCase();
+  
+  const capitalizeWords = (str: string): string => {
+    if (str.length === 0) {
+      return '';
+    }
+    const strArray: string[] = str.split(' ');
+    return strArray.map(capitalize).join(' ');
+  };
+  
+  const capitalizeHeadline = (str: string): string => {
+    const outliers: string[] = [
+      'the',
+      'in',
+      'a',
+      'an',
+      'and',
+      'but',
+      'for',
+      'at',
+      'by',
+      'from',
+    ];
+    const strArray: string[] = str.split(' ');
+    const firstWord: string = capitalize(strArray[0]);
+    const restOfWords: string = strArray
+      .slice(1)
+      .map((word) => (outliers.indexOf(word) !== -1 ? word : capitalize(word)))
+      .join(' ');
+  
+    return [firstWord, restOfWords].join(' ');
+  };
+  
+  const removeExtraSpaces = (str: string): string => {
+    const trimmedStr: string = str.trim();
+    return trimmedStr.split(/\s+/).join(' ');
+  };
+  
+  const kebobCase = (str: string, delimiter: string = '-'): string => {
+    const goodCodes: number[] = [
+      32, 45, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 97, 98, 99, 100, 101, 102,
+      103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117,
+      118, 119, 120, 121, 122,
+    ];
+    const lowerString: string = str.toLowerCase();
+    const splitStr: string[] = lowerString.split('');
+    const cleanStr: string[] = [];
+    for (let i = 0; i < splitStr.length; i += 1) {
+      if (goodCodes.indexOf(splitStr.join('').charCodeAt(i)) !== -1) {
+        cleanStr.push(splitStr[i]);
+      }
+    }
+    return removeExtraSpaces(cleanStr.join('')).split(' ').join(delimiter);
+  };
+  
+  const snakeCase = (str: string): string => kebobCase(str, '_');
+  
+  const camelCase = (str: string): string => {
+    const strArray: string[] = str.trim().split(' ');
+    const firstWord: string = strArray[0].toLowerCase();
+    const restOfWords: string = strArray.slice(1).map(capitalize).join('');
+    return [firstWord, restOfWords].join('');
+  };
+  
+  const shift = (str: string, numChars: number): string => str.slice(numChars) + str.slice(0, numChars);
+  
+  const makeHashTag = (str: string): string[] => {
+    const sortedWords: string[] = str
+      .split(' ')
+      .sort((a, b) => b.length - a.length || a.localeCompare(b));
+    if (sortedWords.length < 3) {
+      return sortedWords.map((word) => `#${capitalize(word.toLowerCase())}`);
+    }
+    return sortedWords
+      .slice(0, 3)
+      .map((word) => `#${capitalize(word.toLowerCase())}`);
+  };
+  
+  const isEmpty = (str: string): boolean => {
+    const trimmedStr:string = str.trim();
+    return trimmedStr.split(/\s+/).join('').length === 0;
+  };
+  
+  module.exports.capitalize = capitalize;
+  module.exports.allCaps = allCaps;
+  module.exports.capitalizeWords = capitalizeWords;
+  module.exports.capitalizeHeadline = capitalizeHeadline;
+  module.exports.removeExtraSpaces = removeExtraSpaces;
+  module.exports.kebobCase = kebobCase;
+  module.exports.snakeCase = snakeCase;
+  module.exports.camelCase = camelCase;
+  module.exports.shift = shift;
+  module.exports.makeHashTag = makeHashTag;
+  module.exports.isEmpty = isEmpty;
+  
